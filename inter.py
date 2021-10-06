920517e9ed68d89ac069c89891d0436a3763da7c
shailajaBegari <89687827+shailajaBegari@users.noreply.github.com>
2021-10-06 14:52:24 +0530

    Add files via upload

---
+++ inter.py
@@ -0,0 +1,143 @@
+# # i=1
+# # b=[]
+# # while i<=12:
+# #     b.append([i])
+# #     i=i+1
+# # print(b)
+
+
+
+# # for count in range(1,10):
+# #     print(count)
+# #     count=count+1
+
+
+# user = int(input("enter number::"))
+# count = 2
+# while(count<user):
+#     count = count + 1
+#     if count % 5 == 0:
+#         print ("wow!! lots of chocolates")
+#         count = count + 1
+#         break
+#     elif count % 3 == 0:
+#         print ("choco")
+#     elif count % 7 == 0:
+#         print ("late")
+#     elif count % 11 == 0:
+#         print ("chocolate")
+#     else:
+#         print("not here")
+#     count = count + 3
+# Count = count + 1
+# print(count)
+# # (ii) What will be the output if user = 15?
+# user = int(input("hhj"))
+# count = 2
+# while(count<user):
+#     count = count + 1
+#     if count % 5 == 0:
+#         print ("wow!! lots of chocolates")
+#         count = count + 1
+#         break
+#     if count % 3 == 0:
+#         print ("choco")
+#     if count % 7 == 0:
+#         print ("late")
+#     if count % 11 == 0:
+#         print ("chocolate")
+#     else:
+#         print("not here")
+#     count = count + 3
+# Count = count + 1
+# print (count)
+
+# i=0
+# while i<=5:
+#     i=i+1
+#     if i==3:
+#         continue
+#     else:
+#         print(i)
+#########################
+
+# i=0
+# while i<=5:
+#     i=i+1
+#     if i==3:
+#        print("hi")
+#     elif i==5:
+#         continue
+#     else:
+#         print(i)
+
+
+
+
+# name=input("enter name:")
+# handle=open("shailu.txt","r")
+# counts=dict()
+# for line in handle:
+#     words=line.split()
+#     for word in words:
+#         counts[word]=counts.get(word,0)+1
+# bigcount=None
+# bigword=None
+# for word,count in counts.items():
+#     if bigcount is None or count>bigcount:
+#         bigcount=count
+#         bigword=word
+# print(bigword,bigcount)
+
+
+# print(4//2)
+
+
+# x=5
+# if x<2:
+#     print("ok")
+# elif x<10:
+#         print("nbhh")
+# elif x<70:
+#         print("ok")
+
+# for i in range(5):
+#     print(i)
+#     if i>2:
+#         print("mnmdf")
+#     print("hu")
+# print("kkk")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
