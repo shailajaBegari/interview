dbc50f537944fa33cfdd243403d474b05fe6f051
shailajaBegari <89687827+shailajaBegari@users.noreply.github.com>
2021-10-06 15:26:17 +0530

    Add files via upload

---
+++ interviw.py
@@ -0,0 +1,73 @@
+# def f(*a):
+#     print(a)
+# f(1,2,3)
+
+
+
+# d={1:1,2:3,3:4,4:2,5:3}
+# l=[]
+# for i  in d:
+#     for j in d:
+#         if  d[i]==d[j] and i!=j:
+#             l.append(i)
+# print(l)
+
+# n=int(input("enter number:"))
+# # first=n%10
+# second=n//10
+# third=second%10
+# if third==5:
+#     print('yes')
+# else:
+#     print('no')
+
+
+
+# print(356//2)    
+
+
+
+# l=input("enter numbers")
+# a=l.split(",")
+# lis=[]
+# # print(a)
+# for i in a:
+#     b=int(i)
+#     lis.append(i)
+# print(lis)
+
+    
+# i=0
+# c=0
+# j=lis[i]
+# while i<len(lis):
+#     if j<lis[i]:
+#         c+=1
+#     i=i+1
+# print(c)
+
+
+
+
+
+
+
+# def add(n1,n2):
+#     return n1,n2
+# print(add(2,4))
+
+# def one(n):
+#     if n==1:
+#         return 1
+#     return  one(n-1)
+# print(one(5))
+# # 
+
+
+# l=1,4,6,8,9
+# b=len(l)
+# d=(b+1)/2
+# c=int(d)-1
+# print(l[c])
+
+
