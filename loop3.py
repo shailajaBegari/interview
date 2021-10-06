100479fec75e7f9650f1be6185ed8c272f1b8380
shailajaBegari <89687827+shailajaBegari@users.noreply.github.com>
2021-10-06 15:30:01 +0530

    Add files via upload

---
+++ loop3.py
@@ -0,0 +1,35 @@
+a=[1,2,3,4,5,6,"a","b",9]
+# i=-2
+# while i<=-1:
+#     print(a[i])
+#     i=i+1
+
+# print("***********************")
+# print(a[-2])
+# print(a[-1])
+    
+
+
+n=int(input("enter number:"))
+i=0
+while i<n:  
+    print(a[i-n])
+    i=i+1
+
+
+
+
+# a="shaiuj"
+# b="nanikhd"
+# # c=[]
+# c=""
+# i=0
+# while i<len(a):
+#     j=0
+#     while j<len(b):
+#         if a[i]==b[j]:
+#             # c.append(a[i])
+#             c=c+a[i]
+#         j=j+1
+#     i=i+1
+# print(c)
