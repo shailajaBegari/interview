b96e1ed7bdeb1b0feb5bffaf59a5c5fecb34b0cd
shailajaBegari <89687827+shailajaBegari@users.noreply.github.com>
2021-10-06 15:25:32 +0530

    Add files via upload

---
+++ interview.py
@@ -0,0 +1,79 @@
+import sys
+print(sys.getrecursionlimit())
+def fun():
+    print("hello")
+# fun()
+print(fun())
+# 
+# str1="My position is s1t and my 5friend come on 5th"
+# sum=0
+# for i in str1:
+#     b=i.isdigit()
+#     if b==True:
+#         c=int(i)
+#         sum=sum+c
+#     else:
+#         pass
+# print(sum)
+# # ########################################################
+
+
+# sum=0
+# d="123456789"
+# i=0
+# while i<len(str1):
+#     if str1[i] in d:
+#         sum=sum+int(str1[i])
+#     i=i+1
+# print(sum)
+
+# # output : 5
+
+
+# a=input("enter any string::")
+# b =input("enter letter::")
+# i=0
+# s=""
+# n="_"
+# while i<len(a):
+#     if a[i]==b:
+#         s=s+n
+#     else:
+#         s=s+a[i]
+#     i=i+1
+# print(s)
+
+
+# x="4.5"
+# s=float(x)
+# r=int(s)
+# # print(r)
+# y=4.5
+# # print(r+y)
+# # c=int(y)
+# # print(c)
+# z="7"
+# r=int(z)
+# p=2
+# print(x+z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
