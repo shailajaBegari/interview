4c1e90eb19b49db7682692fc75177baa7ab0f484
shailajaBegari <89687827+shailajaBegari@users.noreply.github.com>
2021-10-06 14:53:23 +0530

    Add files via upload

---
+++ tryexcept.py
@@ -0,0 +1,59 @@
+# raw=input("enter number:")
+# try:
+#     val=int(raw)
+# except:
+#     val=-2
+# if val>0:
+#     print("nice")
+# else:
+#     print("not a number")
+
+# x=max("hello")
+# print(x)
+
+# while True:
+#     line=input("enter strings::")
+#     if line=="done":
+#         # print("bijji")
+#         break
+#     print(line)
+# print("nani")
+
+
+
+
+# while True:
+#     line=input("enter strings::")
+#     if line=="done":
+#         print("nuanu")
+#         continue
+#     if line=="bujii":
+#         break
+#     print(line)
+# print("nani")
+
+
+# def add(a,b):
+#     add=a+b
+#     return add
+# def sub(a,b):
+#     sub=a-b
+#     return sub
+# def  mul(a,b):
+#     mul =a*b
+#     return mul
+# def div (a,b):
+#     div=a%b
+#     return div
+# def main():
+#     print(add(n,m),sub(n,m),mul(n,m),div(n,m))
+# n=int(input("enter number:"))
+# m=int(input("enter number:"))
+# main()
+
+# x=["nani","bujji"]
+# y=["nani","bujji"]
+# z=x
+# print(type(z))
+# print((x is z))
+# print(x is y)
